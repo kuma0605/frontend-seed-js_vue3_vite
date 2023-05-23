381620a9135b372a1d792ffd8641769df56ea46a
dylan <825880445@qq.com>
2023-05-23 17:29:39 +0800

    unocss tailwind

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
-import './assets/main.css'
+import './assets/customMain.css'
+
+import 'virtual:uno.css'
+
+import '@unocss/reset/tailwind-compat.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
