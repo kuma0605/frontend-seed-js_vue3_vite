68e4d9ff8cfdfe73e40ad3257e7d27c63e0abcca
dylan <825880445@qq.com>
2023-05-24 09:24:50 +0800

    完善环境变量配置

--- vite.config.js
+++ vite.config.js
@@ -1,60 +1,62 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig,loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import postCssPxToRem from "postcss-pxtorem"
 import UnoCSS from 'unocss/vite'
 
-let serverUrl = import.meta.env.VITE_APP_API_BASE_URL;
-
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue(), vueJsx(), UnoCSS(),],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  css: {
-    postcss: {
-      plugins: [
-        postCssPxToRem({
-          rootValue: 16, // 1rem的大小
-          propList: ['*'], // 需要转换的属性，这里选择全部都进行转换
-        })
-      ]
-    }
-  },
-  server:{
-    host:"0.0.0.0",
-    port:5173,
-    proxy:{
-      // 带选项写法：http://localhost:5173/api/bar -> http://jsonplaceholder.typicode.com/bar
-      
-      '/sys': {
-        target: serverUrl,
-        changeOrigin: true
-      },
-      '/study': {
-        target: serverUrl,
-        changeOrigin: true
-      },
-      '/business': {
-        target: serverUrl,
-        changeOrigin: true
-      },
-      '/kh': {
-        target: serverUrl,
-        changeOrigin: true
-      },
-      '/statics': {
-        target: serverUrl,
-        changeOrigin: true
-      },
-      '/logout': {
-        target: serverUrl,
-        changeOrigin: true
+export default defineConfig(({mode})=>{
+  const env = loadEnv(mode, process.cwd(),""); 
+  let serverUrl = env.VITE_API_BASE_URL;
+  return {
+    plugins: [vue(), vueJsx(), UnoCSS(),],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    css: {
+      postcss: {
+        plugins: [
+          postCssPxToRem({
+            rootValue: 16, // 1rem的大小
+            propList: ['*'], // 需要转换的属性，这里选择全部都进行转换
+          })
+        ]
+      }
+    },
+    server:{
+      host:"0.0.0.0",
+      port:5173,
+      proxy:{
+        // 带选项写法：http://localhost:5173/api/bar -> http://jsonplaceholder.typicode.com/bar
+        
+        '/sys': {
+          target: serverUrl,
+          changeOrigin: true
+        },
+        '/study': {
+          target: serverUrl,
+          changeOrigin: true
+        },
+        '/business': {
+          target: serverUrl,
+          changeOrigin: true
+        },
+        '/kh': {
+          target: serverUrl,
+          changeOrigin: true
+        },
+        '/statics': {
+          target: serverUrl,
+          changeOrigin: true
+        },
+        '/logout': {
+          target: serverUrl,
+          changeOrigin: true
+        }
       }
     }
   }
