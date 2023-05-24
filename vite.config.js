abf4a9c37f567ef6b14b197a3997610944ba43b7
dylan <825880445@qq.com>
2023-05-24 09:06:43 +0800

    添加环境变量

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,8 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 import postCssPxToRem from "postcss-pxtorem"
 import UnoCSS from 'unocss/vite'
 
+let serverUrl = import.meta.env.VITE_APP_API_BASE_URL;
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx(), UnoCSS(),],
@@ -24,4 +26,36 @@ export default defineConfig({
       ]
     }
   },
+  server:{
+    host:"0.0.0.0",
+    port:5173,
+    proxy:{
+      // 带选项写法：http://localhost:5173/api/bar -> http://jsonplaceholder.typicode.com/bar
+      
+      '/sys': {
+        target: serverUrl,
+        changeOrigin: true
+      },
+      '/study': {
+        target: serverUrl,
+        changeOrigin: true
+      },
+      '/business': {
+        target: serverUrl,
+        changeOrigin: true
+      },
+      '/kh': {
+        target: serverUrl,
+        changeOrigin: true
+      },
+      '/statics': {
+        target: serverUrl,
+        changeOrigin: true
+      },
+      '/logout': {
+        target: serverUrl,
+        changeOrigin: true
+      }
+    }
+  }
 })
